b1ca9b0098f42cadb2c22977de3b81d7869453bc
adi123q <sobieski123q@gmail.com>
2015-12-19 11:57:22 +0100

    Silnia
    Liczę silnie z dowolnej liczby

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace silnia
+{
+    
+    class Program
+    {
+        public static int Silnie(int a)
+        {
+            int suma = 1, silnia = 1, i = 1;
+
+            while (a>i)
+            {
+                suma = a;
+                silnia *= suma;
+                a--;
+            }
+            return silnia;
+        }
+
+        static void Main(string[] args)
+        {
+            for (;;)
+            {
+                int b;
+                Console.WriteLine("Podaj z jakiej liczby chcesz obliczyc silnie: ");
+                b = int.Parse(Console.ReadLine());
+                Console.WriteLine("SILNIA WYNOSI: "+Silnie(b));
+                
+            }
+        }
+        
+    }
+}
